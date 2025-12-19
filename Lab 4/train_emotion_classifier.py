75839416e01e4f9c462bee02f3b4951ef31464b8
scpf-o5-7 <wangez2025@shanghaitech.edu.cn>
2025-12-19 11:09:22 +0800

    Lab 4 Commit 1.2

--- Lab 4/train_emotion_classifier.py
+++ Lab 4/train_emotion_classifier.py
@@ -6,17 +6,20 @@
 import torch
 import my_net
 
+from model import emotionNet
+
 batchsize    = 32
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-#If you have a Mac
-#device = torch.device('mps' if torch.cuda.is_available() else 'cpu')
 print("Training Device: ", device)
 
 # Load dataset
 train_loader, classes= my_net.utility.loadTrain("./images", batchsize)
 
 # Set model, lossfunc and optimizer
-model,lossfun,optimizer = my_net.classify.makeEmotionNet(False)
+model = emotionNet(printtoggle=True)
+model = model.to(device)
+lossfun = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # Start training process
 losses, accuracy, _ = my_net.utility.function2trainModel(model, device, train_loader, lossfun, optimizer)
